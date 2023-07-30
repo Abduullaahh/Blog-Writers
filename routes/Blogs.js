1139d044c5408e5a63cf90158d83859c35597017
Abdullah Tahir <108390173+Abduullaahh@users.noreply.github.com>
2023-07-31 01:19:04 +0500

    Update Blogs.js

--- routes/Blogs.js
+++ routes/Blogs.js
@@ -34,7 +34,7 @@ router.post
             else
             {
                 console.log('Data Saved');
-                res.redirect=('http://localhost:3000/Create')
+                res.redirect=('blogs-writer.netlify.app')
             }
         });
     }
