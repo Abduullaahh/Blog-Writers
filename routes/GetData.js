c8a1cdb50d03dc02e55f74406dae5bac26a80e85
Abdullah Tahir <108390173+Abduullaahh@users.noreply.github.com>
2023-07-31 10:50:34 +0500

    Update GetData.js

--- routes/GetData.js
+++ routes/GetData.js
@@ -2,28 +2,15 @@ const express = require("express");
 const router = express.Router();
 const {connection} = require('../database/db')
 
-// router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
   
-//     const queryResult = await new Promise((resolve, reject) => {
-//       connection.query('SELECT * FROM blogs', (err, rows) => {
-//         if (err) reject(err);
-//         resolve(rows);
-//       });
-//     });
-//     res.json(queryResult);
-// });
-
-// module.exports = router;
-
-router.get('/', (req, res) => {
-    connection.query('SELECT * FROM blogs', (err, result) => {
-        if (err) {
-            console.error('Error executing SQL query:', err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.send(result);
-        }
+    const queryResult = await new Promise((resolve, reject) => {
+      connection.query('SELECT * FROM blogs', (err, rows) => {
+        if (err) reject(err);
+        resolve(rows);
+      });
     });
+    res.json(queryResult);
 });
 
-module.exports = router;
+module.exports = router;
